a58d373a768de1ba74ad854d7a4362d6e8359157
SwampBlimps Laptop2 <swampblimps@gmail.com>
2023-05-19 19:09:34 -0400

    Delete Micro ROS

--- Summer2023/microros_ws/build/micro_ros_msgs/micro_ros_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/corelab-laptop2/microros_ws/build/micro_ros_msgs/micro_ros_msgs__py
-# Build directory: /home/corelab-laptop2/microros_ws/build/micro_ros_msgs/micro_ros_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
