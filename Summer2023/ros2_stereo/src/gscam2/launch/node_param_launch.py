8a57a4aa66ac173876ec1b813c3c9be701921cd8
swampblimps <swampblimps@gmail.com>
2023-11-13 18:16:36 -0500

    vision

--- Summer2023/ros2_stereo/src/gscam2/launch/node_param_launch.py
+++ Summer2023/ros2_stereo/src/gscam2/launch/node_param_launch.py
@@ -4,10 +4,12 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 # Your camera namespace
-camera_name = 'my_camera'
+# camera_name = 'BurnCreamBlimp'
 
 # Location of configuration directory
 config_dir = os.path.join(get_package_share_directory('gscam2'), 'cfg')
@@ -23,27 +25,33 @@ print(camera_config)
 
 
 def generate_launch_description():
-
-    node = Node(
-        package='gscam2',
-        executable='gscam_main',
-        output='screen',
-        name='gscam_publisher',
-        namespace=camera_name,
-        parameters=[
-            # Some parameters from a yaml file
-            params_file,
-            # A few more parameters
-            {
-                'camera_name': camera_name,  # Camera Name
-                'camera_info_url': camera_config,  # Camera calibration information
-            },
-        ],
-        # Remap outputs to the correct namespace
-        remappings=[
-            ('/image_raw', '/' + camera_name + '/image_raw'),
-            ('/camera_info', '/' + camera_name + '/camera_info'),
-        ],
-    )
-
-    return LaunchDescription([node])
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'camera_name',
+            default_value='my_namespace',
+            description='Namespace for the node'
+        ),
+        Node(
+            package='gscam2',
+            executable='gscam_main',
+            output='screen',
+            name='gscam_publisher',
+            namespace=LaunchConfiguration('camera_name'),
+            parameters=[
+                {'gscam_config': LaunchConfiguration('gscam_config')},
+                # Some parameters from a yaml file
+                params_file,
+                # A few more parameters
+                {
+                    'camera_name': LaunchConfiguration('camera_name'),  # Camera Name
+                    'camera_info_url': camera_config,  # Camera calibration information
+                    
+                },
+            ],
+            # Remap outputs to the correct namespace
+            remappings=[
+                (['/', LaunchConfiguration('camera_name'), '/image_raw'], ['/', LaunchConfiguration('camera_name'), '/sync/image_raw']),
+                (['/', LaunchConfiguration('camera_name'), '/camera_info'], ['/', LaunchConfiguration('camera_name'), '/camera_info']),
+            ],
+        ),
+    ])
--- scripts/blimp_2_vision.sh
+++ scripts/blimp_2_vision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 source $CATCHING_BLIMP_PATH/Summer2023/ros2_stereo/install/setup.bash
+source /home/corelab/Luke_ML_Blimp/ros2_tracks_ws/install/setup.bash
 
 ros2 launch stereo_combined blimp_vision.launch.xml blimp_name:=SillyAhBlimp camera_id:=2
