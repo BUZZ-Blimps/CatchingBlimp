8965c8a37a86837e6bfe575df17c36dbcf255c89
buzzblimps <buzzblimps@gmail.com>
2024-04-02 15:32:46 -0400

    Spring 2024 folder, with Teensy_BlinkyLED and Teensy_DesktopMotorTesting projects

---
+++ Spring2024/Teensy_BlinkyLED/src/main.cpp
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+
+void setup(){
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop(){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+}
