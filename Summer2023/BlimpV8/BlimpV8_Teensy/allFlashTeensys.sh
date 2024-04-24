162d337266b763db90a0118bc7ac934264a67a58
Sahaj Patel <74474849+sah4jpatel@users.noreply.github.com>
2024-04-24 14:04:24 -0400

    Update allFlashTeensys.sh

--- Summer2023/BlimpV8/BlimpV8_Teensy/allFlashTeensys.sh
+++ Summer2023/BlimpV8/BlimpV8_Teensy/allFlashTeensys.sh
@@ -1,42 +1,50 @@
 # Ping the Orange Pi's
-if ping -c 1 orangepi1 &> /dev/null; then
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <Path to firmware.hex>"
+    exit 1
+fi
+
+hexFilePath=$1
+
+if ping -c 1 192.168.0.101 &> /dev/null; then
   echo "Orange Pi 1 is online. Flashing..."
-  ./flashTeensy.sh 1
+  ./flashTeensy.sh 1 "$hexFilePath"
 else
   echo "Orange Pi 1 offline."
 fi
 
-if ping -c 1 orangepi2 &> /dev/null; then
+if ping -c 1 192.168.0.102 &> /dev/null; then
   echo "Orange Pi 2 is online. Flashing..."
-  ./flashTeensy.sh 2
+  ./flashTeensy.sh 2 "$hexFilePath"
 else
   echo "Orange Pi 2 offline."
 fi
 
-if ping -c 1 orangepi3 &> /dev/null; then
+if ping -c 1 192.168.0.103 &> /dev/null; then
   echo "Orange Pi 3 is online. Flashing..."
-  ./flashTeensy.sh 3
+  ./flashTeensy.sh 3 "$hexFilePath"
 else
   echo "Orange Pi 3 offline."
 fi
 
-if ping -c 1 orangepi4 &> /dev/null; then
+if ping -c 1 192.168.0.104 &> /dev/null; then
   echo "Orange Pi 4 is online. Flashing..."
-  ./flashTeensy.sh 4
+  ./flashTeensy.sh 4 "$hexFilePath"
 else
   echo "Orange Pi 4 offline."
 fi
 
-if ping -c 1 orangepi5 &> /dev/null; then
+if ping -c 1 192.168.0.105 &> /dev/null; then
   echo "Orange Pi 5 is online. Flashing..."
-  ./flashTeensy.sh 5
+  ./flashTeensy.sh 5 "$hexFilePath"
 else
   echo "Orange Pi 5 offline."
 fi
 
-if ping -c 1 orangepi6 &> /dev/null; then
+if ping -c 1 192.168.0.106 &> /dev/null; then
   echo "Orange Pi 6 is online. Flashing..."
-  ./flashTeensy.sh 6
+  ./flashTeensy.sh 6 "$hexFilePath"
 else
   echo "Orange Pi 6 offline."
 fi
