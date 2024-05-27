14869d1b0a5cc40b5cfbfa4cf5c997c27d81a2e2
buzzblimps <buzzblimps@gmail.com>
2024-05-27 16:41:39 -0400

    i2c comms working
    Got i2c working, now need to make data read look like its supposed to. WiringI2C only lets you read 2 bytes  from a register, but imu needs you to read 6 bytes

--- Summer2024/imu_testing/main.c
+++ Summer2024/imu_testing/main.c
@@ -19,6 +19,7 @@ To save an ssh password for Orange Pi number #: ssh-copy-id opi@opi#
 #include <stdbool.h>
 #include "LSM6DSL.h"
 #include "LIS3MDL.h"
+#include "BM388.h"
 
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
@@ -44,13 +45,24 @@ To save an ssh password for Orange Pi number #: ssh-copy-id opi@opi#
 // static pwm_info pwm_info_t;
 
 int main (int argc, char *argv[]){
-	int LIS3MDL = wiringPiI2CSetup(LIS3MDL_ADDRESS);
-	printf("i2c LIS3MDL: %d\n", LIS3MDL);
+	const char *device;
+	device = "/dev/i2c-3"; 
+
+	int LIS3MDL = wiringPiI2CSetupInterface(device, LIS3MDL_ADDRESS);
+	printf("i2c LIS3MDL fd: %d\n", LIS3MDL);
 	// delay(100);
 
-	int LSM6DSL = wiringPiI2CSetup(LSM6DSL_ADDRESS);
-	printf("i2c LSM6DSL: %d\n", LSM6DSL);
+	int LSM6DSL = wiringPiI2CSetupInterface(device, LSM6DSL_ADDRESS);
+	printf("i2c LSM6DSL fd: %d\n", LSM6DSL);
+
+	int BM388 = wiringPiI2CSetupInterface(device, BM388_ADDRESS);
+	printf("i2c LSM6DSL fd: %d\n", BM388);
 	// delay(100);
+
+	int accRaw[3];
+	float AccXraw; 
+    float AccYraw; 
+    float AccZraw;
 
 	// printf("i2c read LSM6DSL: %d\n", wiringPiI2CRead(LSM6DSL));
 	
@@ -59,8 +71,35 @@ int main (int argc, char *argv[]){
 	wiringPiI2CWriteReg8(LSM6DSL, LSM6DSL_CTRL3_C, 0b01000100);
 	wiringPiI2CWriteReg8(LSM6DSL, LSM6DSL_CTRL2_G, 0b10011100);
 
+	wiringPiI2CWriteReg8(LIS3MDL, LIS3MDL_CTRL_REG1, 0b11011100);     // Temp sesnor enabled, High performance, ODR 80 Hz, FAST ODR disabled and Selft test disabled.
+	wiringPiI2CWriteReg8(LIS3MDL, LIS3MDL_CTRL_REG2, 0b00100000);     // +/- 8 gauss
+	wiringPiI2CWriteReg8(LIS3MDL, LIS3MDL_CTRL_REG3, 0b00000000); 
+
+	wiringPiI2CWriteReg8(BM388,PWR_CTRL, 0b00110011);     // Enables pressure sensor, Enables temperature sensor, Normal mode
+	wiringPiI2CWriteReg8(BM388,OSR, 0b00001100);                 // x16 oversampling pressure measurement, x2 oversampling temp measurement
+	wiringPiI2CWriteReg8(BM388,ODR, 0x03);                 // Output data rate 25Hz
+	wiringPiI2CWriteReg8(BM388,CONFIG, 0b00000110);      // IIR filter coefficient of 63 
+
 	while(true) {
-		printf("i2c LSM6DSL: %d\n", wiringPiI2CReadReg16(LSM6DSL, LSM6DSL_OUT_X_L_XL)); 
+		int out = wiringPiI2CReadReg16(LSM6DSL, LSM6DSL_OUT_X_L_XL); 
+		accRaw[0] = out & 0xFFFF;
+		accRaw[1] = out>>16 & 0xFFFF;
+		accRaw[2] = out>>32 & 0xFFFF;
+
+		if (accRaw[0] >= 32768) accRaw[0] = accRaw[0] - 65536;
+		if (accRaw[1] >= 32768) accRaw[1] = accRaw[1] - 65536;
+		if (accRaw[2] >= 32768) accRaw[2] = accRaw[2] - 65536;
+
+		AccYraw = (accRaw[0] * 0.244) / 1000.0;
+		AccXraw = -(accRaw[1] * 0.244) / 1000.0;
+		AccZraw = (accRaw[2] * 0.244) / 1000.0;
+
+		printf("RAW: %d\n", out);
+		printf("AccYraw: %f\n", AccYraw);
+		printf("AccXraw: %f\n", AccXraw);
+		printf("AccZraw: %f\n", AccZraw);
+
+
 		delay(100);
 	}
 
