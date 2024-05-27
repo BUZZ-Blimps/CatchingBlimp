db88fe3610954178603468749120eae2be0d5233
buzzblimps <buzzblimps@gmail.com>
2024-05-27 10:14:46 -0400

    added while loop to main, deleted compile command for file that doesn't exist in folder

--- Summer2024/imu_testing/main.c
+++ Summer2024/imu_testing/main.c
@@ -23,33 +23,33 @@ To save an ssh password for Orange Pi number #: ssh-copy-id opi@opi#
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
-typedef struct {
-	unsigned int ccr;		// Capture/Compare Register (Duty Cycle)
-	unsigned int arr;		// Auto-Reload Register (Period)
-	unsigned int div;
-	unsigned int div_stepping;
-} pwm_info;
+// typedef struct {
+// 	unsigned int ccr;		// Capture/Compare Register (Duty Cycle)
+// 	unsigned int arr;		// Auto-Reload Register (Period)
+// 	unsigned int div;
+// 	unsigned int div_stepping;
+// } pwm_info;
 
-#define L	0
-#define R	2
-#define BL	5
-#define BR	8
+// #define L	0
+// #define R	2
+// #define BL	5
+// #define BR	8
 
-// Far ranges = [70,528]
-const int pwm_lower = 80;
-const int pwm_upper = 510;
-const int B_pwm_lower = 150;
-const int B_pwm_upper = 200;
+// // Far ranges = [70,528]
+// const int pwm_lower = 80;
+// const int pwm_upper = 510;
+// const int B_pwm_lower = 150;
+// const int B_pwm_upper = 200;
 
-static pwm_info pwm_info_t;
+// static pwm_info pwm_info_t;
 
 int main (int argc, char *argv[]){
 	int LIS3MDL = wiringPiI2CSetup(LIS3MDL_ADDRESS);
-	// printf("i2c LIS3MDL: %d\n", LIS3MDL);
+	printf("i2c LIS3MDL: %d\n", LIS3MDL);
 	// delay(100);
 
 	int LSM6DSL = wiringPiI2CSetup(LSM6DSL_ADDRESS);
-	// printf("i2c LSM6DSL: %d\n", LSM6DSL);
+	printf("i2c LSM6DSL: %d\n", LSM6DSL);
 	// delay(100);
 
 	// printf("i2c read LSM6DSL: %d\n", wiringPiI2CRead(LSM6DSL));
@@ -59,7 +59,12 @@ int main (int argc, char *argv[]){
 	wiringPiI2CWriteReg8(LSM6DSL, LSM6DSL_CTRL3_C, 0b01000100);
 	wiringPiI2CWriteReg8(LSM6DSL, LSM6DSL_CTRL2_G, 0b10011100);
 
-	printf("i2c LIS3MDL: %d\n", wiringPiI2CReadReg8(LIS3MDL, LSM6DSL_OUT_X_L_XL)); 
+	while(true) {
+		printf("i2c LSM6DSL: %d\n", wiringPiI2CReadReg16(LSM6DSL, LSM6DSL_OUT_X_L_XL)); 
+		delay(100);
+	}
+
+	
 
 
 
