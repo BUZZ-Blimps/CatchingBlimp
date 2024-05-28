e22309e50ddd5ee3cff0813ffc409a666f517f54
buzzblimps <buzzblimps@gmail.com>
2024-05-28 12:52:48 -0400

    maybe got accurate imu values on opi, need to validate the acceleration and also clean up

--- Summer2024/imu_testing/main.c
+++ Summer2024/imu_testing/main.c
@@ -21,28 +21,100 @@ To save an ssh password for Orange Pi number #: ssh-copy-id opi@opi#
 #include "LIS3MDL.h"
 #include "BM388.h"
 
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <asm/ioctl.h>
+#include <stdint.h>
+// #include <linux/i2c-dev.h>
+// #include <i2c/smbus.h>
+
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
+// #include <wiringPiI2C.c>
+// I2C definitions
 
-// typedef struct {
-// 	unsigned int ccr;		// Capture/Compare Register (Duty Cycle)
-// 	unsigned int arr;		// Auto-Reload Register (Period)
-// 	unsigned int div;
-// 	unsigned int div_stepping;
-// } pwm_info;
+#define I2C_SLAVE	0x0703
+#define I2C_SMBUS	0x0720	/* SMBus-level access */
 
-// #define L	0
-// #define R	2
-// #define BL	5
-// #define BR	8
+#define I2C_SMBUS_READ	1
+#define I2C_SMBUS_WRITE	0
 
-// // Far ranges = [70,528]
-// const int pwm_lower = 80;
-// const int pwm_upper = 510;
-// const int B_pwm_lower = 150;
-// const int B_pwm_upper = 200;
+// SMBus transaction types
 
-// static pwm_info pwm_info_t;
+#define I2C_SMBUS_QUICK		    0
+#define I2C_SMBUS_BYTE		    1
+#define I2C_SMBUS_BYTE_DATA	    2 
+#define I2C_SMBUS_WORD_DATA	    3
+#define I2C_SMBUS_PROC_CALL	    4
+#define I2C_SMBUS_BLOCK_DATA	    5
+#define I2C_SMBUS_I2C_BLOCK_BROKEN  6
+#define I2C_SMBUS_BLOCK_PROC_CALL   7		/* SMBus 2.0 */
+#define I2C_SMBUS_I2C_BLOCK_DATA    8
+
+// SMBus messages
+
+#define I2C_SMBUS_BLOCK_MAX	32	/* As specified in SMBus standard */	
+#define I2C_SMBUS_I2C_BLOCK_MAX	32	/* Not specified but we use same structure */
+
+// Structures used in the ioctl() calls
+
+union i2c_smbus_data
+{
+  uint8_t  byte ;
+  uint16_t word ;
+  uint8_t  block [I2C_SMBUS_BLOCK_MAX + 2] ;	// block [0] is used for length + one more for PEC
+} ;
+
+struct i2c_smbus_ioctl_data
+{
+  char read_write ;
+  uint8_t command ;
+  int size ;
+  union i2c_smbus_data *data ;
+} ;
+
+static inline int i2c_smbus_access_bl (int fd, char rw, uint8_t command, int size, union i2c_smbus_data *data)
+{
+  struct i2c_smbus_ioctl_data args ;
+
+  args.read_write = rw ;
+  args.command    = command ;
+  args.size       = size ;
+  args.data       = data ;
+  return ioctl (fd, I2C_SMBUS, &args) ;
+}
+
+// int wiringPiI2CReadRegBlock (int fd, int reg)
+// {
+//   union i2c_smbus_data data;
+
+//   if (i2c_smbus_access_bl (fd, I2C_SMBUS_READ, reg, I2C_SMBUS_WORD_DATA, &data))
+//     return -1 ;
+//   else
+//     return data.word & 0xFFFF ;
+// }
+
+int wiringPiI2CReadRegBlock (int fd, int reg, uint8_t *buff)
+{
+  union i2c_smbus_data data;
+  if (i2c_smbus_access_bl (fd, I2C_SMBUS_READ, reg, 6, &data))
+    return -1 ;
+  else
+	for (int i = 0; i < 6; i++) {
+		buff[i] = data.block[i + 1];
+	}
+	// buff[0] = data.block[1];
+	// buff[1] = data.block[2];
+	// buff[2] = data.block[3];
+	// buff[3] = data.block[4];
+	// buff[4] = data.block[5];
+	// buff[5] = data.block[6];
+    return data.block[0];
+//   if (i2c_smbus_read_i2c_block_data(fd, I2C_SMBUS_I2C_BLOCK_DATA, reg, 6, &data))
+//     return -1 ;
+//   else
+//     return data.word & 0xFFFF ;
+}
 
 int main (int argc, char *argv[]){
 	const char *device;
@@ -60,6 +132,7 @@ int main (int argc, char *argv[]){
 	// delay(100);
 
 	int accRaw[3];
+	uint8_t buff[6];
 	float AccXraw; 
     float AccYraw; 
     float AccZraw;
@@ -81,10 +154,10 @@ int main (int argc, char *argv[]){
 	wiringPiI2CWriteReg8(BM388,CONFIG, 0b00000110);      // IIR filter coefficient of 63 
 
 	while(true) {
-		int out = wiringPiI2CReadReg16(LSM6DSL, LSM6DSL_OUT_X_L_XL); 
-		accRaw[0] = out & 0xFFFF;
-		accRaw[1] = out>>16 & 0xFFFF;
-		accRaw[2] = out>>32 & 0xFFFF;
+		int out = wiringPiI2CReadRegBlock(LSM6DSL, LSM6DSL_OUT_X_L_XL, buff); 
+		accRaw[0] = (int)(buff[0] | (buff[1] << 8));
+  		accRaw[1] = (int)(buff[2] | (buff[3] << 8));
+  		accRaw[2] = (int)(buff[4] | (buff[5] << 8));
 
 		if (accRaw[0] >= 32768) accRaw[0] = accRaw[0] - 65536;
 		if (accRaw[1] >= 32768) accRaw[1] = accRaw[1] - 65536;
@@ -95,6 +168,7 @@ int main (int argc, char *argv[]){
 		AccZraw = (accRaw[2] * 0.244) / 1000.0;
 
 		printf("RAW: %d\n", out);
+		printf("Test: %d\n", buff[0]);
 		printf("AccYraw: %f\n", AccYraw);
 		printf("AccXraw: %f\n", AccXraw);
 		printf("AccZraw: %f\n", AccZraw);
